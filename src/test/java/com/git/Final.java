1462878bc19176ca14584f2fd9ebff303227f8dd
Ajithkumar170596 <ajithkumar814.ak@gmail.com>
2022-09-19 14:09:43 +0530

    new methods

---
+++ src/test/java/com/git/Final.java
@@ -0,0 +1,15 @@
+package com.git;
+
+public class Final {
+	
+	
+private void city1() {
+System.out.println("cbe");
+}
+private void city2() {
+System.out.println("chennai");
+}
+private void city3() {
+System.out.println("trichy");
+}
+}
